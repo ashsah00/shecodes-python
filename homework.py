d8196976e37306ad8440cbddbbf998e428bd6a1b
ashsah00 <ash.sahin.vz@gmail.com>
2024-09-29 21:47:48 -0400

    update

--- homework.py
+++ homework.py
@@ -1,11 +1,16 @@
 #W3
 city = input("What is the name of the city? ")
-temp = input("What is the temp in Celcius? ")
+temp = input(f'What is the temp(in Celcius) in {city.capitalize()}? ')
+temp = int(temp)
+
 
 def C_to_F(temp):
-    fahrenheit = float(temp) * 9/5 + 32
-    
+    fahrenheit = float(temp) * 9/5 + 32   
     return fahrenheit
 
-rTemp = C_to_F(temp)
-print (f'It is currently {temp}ºC ({round(rTemp)}ºF) in {city}')
+def display(city, temp):
+    rTemp = C_to_F(temp)
+    print (f'It is currently {temp}ºC ({round(rTemp)}ºF) in {city.capitalize()}')
+   
+display(city, temp) 
+
