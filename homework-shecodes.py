4d7676f117f66dbb7115cb7cc82b1f088c52385b
ashsah00 <168297454+ashsah00@users.noreply.github.com>
2024-10-24 15:05:44 -0400

    Week4 Homework

---
+++ homework-shecodes.py
@@ -0,0 +1,28 @@
+#Exercise 1
+weather = {
+    "city":"Lisbon",
+    "country":"Portugal",
+    "temperature":17.94,
+    "humidity":77
+}
+
+print(f'It is {round(weather["temperature"])}°C in {weather["city"]}, {weather["country"]}. The humidity level is {weather["humidity"]}%.')
+
+#Exercise 2
+forecast = {
+    "city":"Lisbon",
+    "county":"Portugal",
+    "daily": [
+        17.76,
+        13.08,
+        12.14,
+        11.25,
+        14.29
+    ]
+}
+
+print(f'The forecast for {forecast["city"]}, {forecast["county"]} for the next five days is:')
+index = 0
+for daily, temp in forecast.items():
+    print(f'Day {index + 1}: {forecast["daily"][index]}')
+    index +=1
