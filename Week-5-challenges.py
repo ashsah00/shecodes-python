a8eff07f19d9c5768fb12ce439d3e7becaf82a66
ashsah00 <168297454+ashsah00@users.noreply.github.com>
2024-11-03 14:24:10 -0500

    Delete Week-5-challenges.py

--- Week-5-challenges.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-#current date and time
-now = datetime.now()
-print(now)
-
-#Date: Jan 12, 2032 Time: 14:03
-formatteddate = now.strftime("%b %d, %Y Time:%I:%M")
-print(formatteddate)
